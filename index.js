7f5add881e71ed47180b96ab902272fbf67a5547
bmdilts <bmdilts@gmail.com>
2017-06-26 17:36:05 -0400

    mostly working, just need to remove commas, lol

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 const session = require('express-session');
 const fs = require('fs');
+const validator = require('express-validator');
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
@@ -14,18 +15,28 @@ app.use(session({
 }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static('public'));
+app.use(validator());
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-const letters = ["none"]
 const word = words[(Math.floor(Math.random() * words.length))];
-var spaces = []
-const displayWord = function(){
-  for(i = 0; i < word.length; i++){
-    spaces.push("_");
-  };
-  return spaces.join(" ");
+const letters = ["none"]
+const wordUp = word.toUpperCase();
+const wordArr = (wordUp.split(""));
+
+
+function x(){
+    spaces = []
+    for(i = 0; i < word.length; i++){
+      spaces.push("_");
+    };
+    displayWord = spaces.join(" ");
+    return spaces;
 };
-const tags = {num: 8, word: displayWord, letters: letters}
+
+x();
+
+const tags = {num: 8, word: spaces, letters: letters}
+
 // var tags = [req.body.num, req.body.word, req.body.letters]
 
 app.get("/", function(req, res){
@@ -35,8 +46,38 @@ app.get("/", function(req, res){
   }
   res.render("home", {tags: tags});
 });
+app.post("/", function(req, res){
+  req.checkBody('input', 'Invalid guess!')
+    .isLength({max: 1})
 
+  const errors = req.validationErrors();
+  var input = req.body.input.toUpperCase();
+  console.log(input);
 
+  if (errors) {
+    res.send(errors);
+  } else if (letters.includes(input)){
+      console.log("Letter already guessed, try again!");
+      input = "";
+      console.log(input)
+  } else if(wordArr.includes(input)){
+    for(let i = 0; i < wordArr.length; i++){
+      if(wordArr[i] === input){
+        spaces.splice(i, 1, input);
+        console.log(spaces);
+        displayWord = spaces.join(" ");
+      }
+    }
+  } else {
+      tags.num--;
+    }
+  if(input !== ""){
+    letters.push(input);
+  }
+  res.redirect('/');
+});
+
+// spaces.splice(wordArr.findIndex(input), 1 input)
 
 
 app.listen(3000, function(){
