49da2264e436cfdae8c4171d0955f1f5ee989a0c
bmdilts <bmdilts@gmail.com>
2017-06-28 15:57:29 -0400

    all basic requirements done, may do hard mode tonight

--- index.js
+++ index.js
@@ -18,42 +18,56 @@ app.use('/public', express.static('public'));
 app.use(validator());
 
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-const word = words[(Math.floor(Math.random() * words.length))];
-const letters = ["none"]
-const wordUp = word.toUpperCase();
-const wordArr = (wordUp.split(""));
-spaces = [];
+// var word = words[(Math.floor(Math.random() * words.length))];
+// const letters = ["none"]
+// const wordUp = word.toUpperCase();
+// const wordArr = (wordUp.split(""));
+// var spaces = [];
 var displayWord = "";
 
-function x(){
-    for(i = 0; i < word.length; i++){
-      spaces.push("_");
-    };
-    return spaces;
-};
-
-x();
+// function x(){
+//     for(i = 0; i < word.length; i++){
+//       spaces.push("_");
+//     };
+//     return spaces;
+// };
+//
+// x();
 // var displayWord = spaces.join(" ");
 
-const tags = {num: 8, word: displayWord, letters: letters};
+// const tags = {num: 8, word: displayWord, letters: letters};
 
 // var tags = [req.body.num, req.body.word, req.body.letters]
 
 app.get("/", function(req, res){
   if(!req.session.word){
+    word = words[(Math.floor(Math.random() * words.length))];
+    function x(){
+      spaces = [];
+      for(i = 0; i < word.length; i++){
+        spaces.push("_");
+      };
+      return spaces;
+    }; x();
+    const wordUp = word.toUpperCase();
+    req.session.wordArr = (wordUp.split(""));
     req.session.word = word;
     req.session.num = 8;
     req.session.letters = [];
+    req.session.win = true;
+    req.session.msg = "";
+    req.session.loserWord = "";
     console.log(req.session.word);
   }
   req.session.displayWord = spaces.join(" ");
-  const tags = {num: req.session.num, word: req.session.displayWord, letters: req.session.letters};
+  const tags = {num: req.session.num, word: req.session.displayWord, letters: req.session.letters, win: req.session.win, msg: req.session.msg, loser: req.session.loser};
   res.render("home", {tags: tags});
 });
 
+
 app.post("/", function(req, res){
   req.checkBody('input', 'Invalid guess!')
-    .isLength({max: 1})
+    .isLength({min: 1, max: 1})
 
   const errors = req.validationErrors();
   var input = req.body.input.toUpperCase();
@@ -62,9 +76,9 @@ app.post("/", function(req, res){
   } else if (req.session.letters.includes(input)){
       console.log("Letter already guessed, try again!");
       input = "";
-  } else if(wordArr.includes(input)){
-    for(let i = 0; i < wordArr.length; i++){
-      if(wordArr[i] === input){
+  } else if(req.session.wordArr.includes(input)){
+    for(let i = 0; i < req.session.wordArr.length; i++){
+      if(req.session.wordArr[i] === input){
         spaces.splice(i, 1, input);
         req.session.displayWord = spaces.join(" ");
       }
@@ -72,22 +86,25 @@ app.post("/", function(req, res){
   } else {
       req.session.num--;
     };
-
   if(input !== ""){
     req.session.letters.push(input);
   };
-
-  if(req.session.num === 0){
-    console.log("You lose!")
-  };
-
   if(!req.session.displayWord.includes("_")){
-    console.log("You win!")
+    req.session.win = false;
+    req.session.msg = "You win!"
+  };
+  if(req.session.num === 0){
+    req.session.win = false;
+    req.session.msg = "You lose!";
+    req.session.loser = req.session.word;
   };
   res.redirect('/');
 });
 
-
+app.post("/again", function(req, res){
+  req.session.word = null;
+  res.redirect('/')
+});
 
 app.listen(3000, function(){
   console.log("Hey, Listen!")
